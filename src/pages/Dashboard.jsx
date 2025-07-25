c3e86babd8137e671c1ecfda0d2164b53c0667ca
Kumar-Omm <omjha1603kumar@gmail.com>
2025-07-25 12:45:32 +0530

    Fixing a Few Things

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+   return (
+      <div className="flex flex-col items-center justify-center h-screen bg-black-200 font-sans text-white text-color  ">
+        <center><h1>ExcelTech Home Page</h1></center>
+      </div>
+     )
+}
+
+export default Dashboard
